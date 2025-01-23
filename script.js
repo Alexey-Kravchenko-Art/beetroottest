c7a8bb7f1bad63dad437b6ccd34bb94edd9b7361
Alexey-Kravchenko-Art <alexias.kravchenko@gmail.com>
2025-01-23 19:16:39 +0200

    Update script.js
    alerts were removed

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function myFunction(){
-		alert('new function version implemented');
 		var a=12*3;
 		var b=100;
 		var c=a*b;
-		alert("second alert result = "  + c)
 	}
 	
 function newFunction(){
