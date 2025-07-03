0c0b61d88819523d460e26f3aaef107003badcca
Alexey-Kravchenko-Art <119417713+Alexey-Kravchenko-Art@users.noreply.github.com>
2025-07-03 20:27:05 +0300

    Update script.js
    alert line uncommented

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function demoBranchFunc(){
 }
 
 function otherFunction(){
-	//alert('new function version implemented');
+	alert('new function version implemented');
 	var a=12*3;
 	var b=100;
 	var c=a*b;
